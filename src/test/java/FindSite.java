56e427edd47bda1b6982480a3812a7b33301724e
Oleksandra_Dmytrenko <oleksandra_dmytrenko1@epam.com>
2017-06-13 19:46:51 +0300

    Added gitignore and driver location

--- src/test/java/FindSite.java
+++ src/test/java/FindSite.java
@@ -14,6 +14,7 @@ import static org.junit.Assert.assertEquals;
 
 /**
  * Created by olexandra on 6/11/17.
+ * Download driver from here: https://github.com/mozilla/geckodriver/releases
  */
 public class FindSite {
     private WebDriver driver;
